ee893457202182d77fbc994a5964389e6785559d
DimkaGlamazda <gldmvl@gmail.com>
2018-11-28 09:35:16 +0200

    Created Database Movie, Session and MovieSession tables

---
+++ OnlineCinema.Database/dbo/Tables/Movie.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Movie] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [GenreId]    INT            NOT NULL,
+    [Name]       NVARCHAR (MAX) NOT NULL,
+    [Image]      NVARCHAR (MAX) NULL,
+    [VideoLink]  NVARCHAR (MAX) NULL,
+    [RentalFrom] DATETIME       NOT NULL,
+    [RentalTo]   DATETIME       NOT NULL,
+    CONSTRAINT [PK_Movie] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ OnlineCinema.Database/dbo/Tables/Session.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Session] (
+    [Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [Title]    NVARCHAR (MAX) NOT NULL,
+    [TimeFrom] TIME (7)       NOT NULL,
+    [TimeTo]   TIME (7)       NOT NULL,
+    CONSTRAINT [PK_Session] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+

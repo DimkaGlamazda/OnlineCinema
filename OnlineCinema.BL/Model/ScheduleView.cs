0028ec9dc14b4a30c10a358c80b91326589d381f
alevel-petrenko <petrenko.anton22@gmail.com>
2018-12-03 01:34:27 +0200

    BussinesLogic with nacked Services

---
+++ OnlineCinema.BL/Model/ScheduleView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineCinema.BL.Model
+{
+    public class ScheduleView
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int MovieId { get; set; }
+
+        public int SessionId { get; set; }
+
+        public virtual MovieView Movie { get; set; }
+
+        public virtual SessionView Session { get; set; }
+    }
+}
---
+++ OnlineCinema.BL/Model/SessionView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineCinema.BL.Model
+{
+    public class SessionView
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public TimeSpan TimeFrom { get; set; }
+
+        public TimeSpan TimeTo { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}

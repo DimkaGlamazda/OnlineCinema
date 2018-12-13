ab5895619f7de673bd10c6253590d24ef0d98049
alevel-petrenko <petrenko.anton22@gmail.com>
2018-12-13 15:06:28 +0200

    implemented MovieMock service and Controller

---
+++ OnlineCinema.API/Controllers/MovieMockController.cs
@@ -0,0 +1,59 @@
+﻿using OnlineCinema.BL.Model;
+using OnlineCinema.BL.Services;
+using OnlineCinema.BL.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Ninject;
+
+namespace OnlineCinema.API.Controllers
+{
+    [Authorize]
+    public class MovieMockController : ApiController
+    {
+        [Inject]
+        private readonly IMovieService _movieService;
+
+        public MovieMockController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            var _moviesList = _movieService.GetAll().Select(b => b.ToViewModel()).OrderBy(f => f.Name);
+
+            return Ok(_moviesList);
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            var movie = _movieService.GetItem(id).ToViewModel();
+
+            return Ok(movie);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Add([FromBody]MovieView movie)
+        {
+            return BadRequest("Invalid endpoint");
+        }
+
+        [HttpPut]
+        public IHttpActionResult Update([FromBody] MovieView model)
+        {
+            return BadRequest("Invalid endpoint");
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            return BadRequest("Invalid endpoint");
+        }
+    }
+}
---
+++ OnlineCinema.BL/Services/MockMovieService.cs
@@ -0,0 +1,132 @@
+﻿using OnlineCinema.BL.Model;
+using OnlineCinema.DB;
+using OnlineCinema.DB.DTOs;
+using OnlineCinema.DB.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineCinema.BL.Services
+{
+    public class MockMovieService : IMovieService
+    {
+        /// <summary>
+        /// 1-Adventure
+        /// 2-Crime
+        /// 3-Action
+        /// 4-Animation
+        /// 5-Drama
+        /// </summary>
+        /// 
+        private List<MovieDto> _mockMovies = new List<MovieDto>
+        {
+            new MovieDto ()
+            {
+                Id = 1,
+                GenreId = 1,
+                Name = "Back to the Future",
+                Image = null,
+                VideoLink = "https://www.imdb.com/title/tt0088763/"
+            },
+            new MovieDto ()
+            {
+                Id = 2,
+                GenreId = 3,
+                Name = "The Dark Knight",
+                Image = null,
+                VideoLink = "https://www.imdb.com/title/tt0468569/"
+            },
+            new MovieDto ()
+            {
+                Id = 3,
+                GenreId = 2,
+                Name = "Pulp Fiction",
+                Image = null,
+                VideoLink = "https://www.imdb.com/title/tt0110912/"
+            },
+            new MovieDto ()
+            {
+                Id = 4,
+                GenreId = 5,
+                Name = "Slumdog Millionaire",
+                Image = null,
+                VideoLink = "https://www.imdb.com/title/tt1010048/"
+            },
+            new MovieDto ()
+            {
+                Id = 5,
+                GenreId = 4,
+                Name = "Monsters, Inc.",
+                Image = null,
+                VideoLink = "https://www.imdb.com/title/tt0198781/"
+            },
+            new MovieDto ()
+            {
+                Id = 6,
+                GenreId = 5,
+                Name = "Bohemian Rhapsodye",
+                Image = null,
+                VideoLink = "https://www.imdb.com/title/tt1727824/"
+            },
+            new MovieDto ()
+            {
+                Id = 7,
+                GenreId = 1,
+                Name = "The Shape of Water",
+                Image = null,
+                VideoLink = "https://www.imdb.com/title/tt5580390/"
+            },
+            new MovieDto ()
+            {
+                Id = 8,
+                GenreId = 2,
+                Name = "The Departed",
+                Image = null,
+                VideoLink = "https://www.imdb.com/title/tt0407887/"
+            },
+            new MovieDto ()
+            {
+                Id = 9,
+                GenreId = 3,
+                Name = "Sherlock Holmes",
+                Image = null,
+                VideoLink = "https://www.imdb.com/title/tt0988045/"
+            },
+            new MovieDto ()
+            {
+                Id = 10,
+                GenreId = 4,
+                Name = "Zootopia",
+                Image = null,
+                VideoLink = "https://www.imdb.com/title/tt2948356/"
+            }
+        };
+
+        public int Add(MovieDto movieDto)
+        {
+            return 0;
+        }
+
+        public void Delete(int id)
+        {
+
+        }
+
+        public List<MovieDto> GetAll()
+        {
+            return _mockMovies.ToList();
+        }
+
+        public MovieDto GetItem(int id)
+        {
+            return _mockMovies[id];
+        }
+
+        public void Update(MovieDto movieDto)
+        {
+
+        }
+    }
+}
